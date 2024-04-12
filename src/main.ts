dffa6c70b035eedadc2438a00af633ea7d0ada80
alejandro-it-goes <alejandro.montepeque@goes.gob.sv>
2024-04-12 15:44:37 -0600

    Adding: añadiendo Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+//import App from './App.vue'
+import Log from './Login.vue'
 
-createApp(App).mount('#app')
+createApp(Log).mount('#app')
